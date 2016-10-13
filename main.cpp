b5c390f3aafc4cc189e356cbd617669a66dda66d
Jian Wei <jianwei@Jians-MacBook-Pro.local>
2016-10-13 01:47:46 -0400

    second edit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
 
+class myInterator {
+public:
+  myIterator(const vector<int>::iterator & it) : myIter(it){}
+
+  myIterator & operator--() {
+    --myIter;
+    --myIter;
+    return myIter;
+  }
+
+  myIterator & operator++() {
+    ++myIter;
+    ++myIter;
+    return myIter;
+  }
+
+  bool operator==(const vector<int>::iterator & other) {
+    return myIter==other;
+  }
+
+  int& operator*() {
+    return *myIter;
+  }
+
+private:
+  vector<int>::iterator myIter;
+};
 
 class solution {
 public:
